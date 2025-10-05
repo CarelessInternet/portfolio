c6111f98a55fa5c720737f499f8397c61aa31e34
CarelessInternet <myinternetisg00d@gmail.com>
2025-10-05 00:03:03 +0000

    feat: umami analytics and update content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: node({ mode: 'standalone' })
+	adapter: node({ mode: 'standalone' }),
+	env: {
+		schema: {
+			UMAMI_URL: envField.string({
+				context: 'client',
+				access: 'public',
+				url: true,
+				optional: true
+			}),
+			UMAMI_ID: envField.string({
+				context: 'client',
+				access: 'public',
+				optional: true
+			})
+		}
+	}
 });
